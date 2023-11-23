e73e17fd56ce551ea6f995aa9c35c7b59b80d11b
Anjela <anjela1996qa@gmail.com>
2023-11-23 12:02:58 +0300

    add example-005-condition_if_else/

---
+++ example-005-condition_if_else/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Enter your name: ");
+string username = Console.ReadLine()!;
+
+if(username == "Masha")
+{
+    Console.WriteLine("EEE, It's Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+
+// if(username.ToLower() == "маша"){
+//     Console.WriteLine("Ура, это же Маша!");
+// }
+// else{
+//     // Console.WriteLine($"Привет, {username}!");
+
+// }
