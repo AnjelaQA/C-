d949fbf98df46ba3d7807132933515a56985a17a
Anjela <anjela1996qa@gmail.com>
2023-11-27 14:36:12 +0300

    add example-008-kratnoeChislo/

---
+++ example-008-kratnoeChislo/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Write you number");
+int n = int.Parse(Console.ReadLine()!);
+
+if(n % 7 == 0 || n % 23 ==0)
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Число не кратно 7 и 23");
+}
